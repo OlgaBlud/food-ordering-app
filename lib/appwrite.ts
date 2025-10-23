f1d461796596ed2bb2fc66031355913bcb4df8ed
Olga Blud <olga.blud@gmail.com>
2025-10-23 13:19:00 +0300

    added sign up & in screens

---
+++ lib/appwrite.ts
@@ -0,0 +1,13 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  //   userCollection: "user",
+  //   bucketId: "bucketId",
+  //   userCollectionId: "userCollectionId",
+  //   categoriesCollectionId: "categoriesCollectionId",
+  //   menuCollectionId: "menuCollectionId",
+  //   customizationsCollectionId: "customizationsCollectionId",
+  //   menuCustomizationsCollectionId: "menuCustomizationsCollectionId",
+};
